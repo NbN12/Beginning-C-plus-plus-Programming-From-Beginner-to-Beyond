d2d1350304721c03aaaf9817d89dd7b770199b7c
Jack Wonder <babyfacedeveloper@gmail.com>
2020-04-30 22:38:16 +0700

    Solution section 19.3

--- Section_19_3/main.cpp
+++ Section_19_3/main.cpp
@@ -2,13 +2,52 @@
 // Challenge 3
 // Word counter
 #include <iostream>
-
+#include <fstream>
+#include <vector>
 using namespace std;
 
-int main() {
-    
-    
-    cout << endl;
-    return 0;
+static vector<string> texts;
+
+void load_text(ifstream &in_file)
+{
+    string temp;
+    while (in_file >> temp)
+        texts.push_back(temp);
 }
 
+static uint32_t search(const string &user_input)
+{
+    uint32_t count = 0;
+    for (const string &text : texts)
+        if (text.find(user_input) != string::npos)
+            ++count;
+    return count;
+}
+
+int main()
+{
+    ifstream in_file;
+    in_file.open("romeoandjuliet.txt");
+    if (!in_file)
+    {
+        cerr << "Can not open file\n";
+        return 1;
+    }
+
+    load_text(in_file);
+
+    string user_input;
+    while (1)
+    {
+        cout << "Enter the substring to search for file \"romeoandjuliet.txt\" or type (q or Q) to quit: ";
+        cin >> user_input;
+        if (user_input == "q" || user_input == "Q")
+            break;
+        uint32_t result = search(user_input);
+        cout << texts.size() << " words were searched...\n";
+        cout << "The substring " << user_input << " was found " << result << " times\n";
+    }
+
+    cout << "GOOD BYE\n";
+    return 0;
+}
